6fbb6a6cef879267ebdd1490540531c89388b5f4
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2023-05-11 22:09:08 -0300

    update

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,18 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'imagens/manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.background = '#F98D26'
+    } else {
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#084CA1'
+    }
+}
