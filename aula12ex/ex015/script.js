454a3dd8fd98531b7f0db31c2550e815f53965a1
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2023-05-16 14:44:25 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,4 +1,8 @@
 function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
-    
+    var anoNasc = document.getElementById('iano')
+    var resp = anoNasc - ano
+    var res = document.getElementById('res')
+    res.innerHTML = `A idade atual é de ${resp}`
+} 
