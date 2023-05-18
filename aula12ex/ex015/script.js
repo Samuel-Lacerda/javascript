7f19e5c314f5b1c59935af81f3530e37f7e34fbb
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2023-05-17 22:05:20 -0300

    update

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,8 +1,48 @@
 function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
-    var anoNasc = document.getElementById('iano')
-    var resp = anoNasc - ano
+    var anoNasc = document.getElementById('iano').value
+    var resp = ano - anoNasc
     var res = document.getElementById('res')
-    res.innerHTML = `A idade atual é de ${resp}`
+    var sexo = document.getElementsByName('radsex')
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id','foto')
+    if (sexo[0].checked){
+            genero = 'Homem'
+            if (resp >= 0 && resp <= 10){
+                //criança
+                img.setAttribute('src', 'imagens/criança-homem.jpg')
+            } else if (resp < 21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem-homem.jpg')
+            } else if (resp < 55){
+                //adulto
+                img.setAttribute('src', 'imagens/jovem-homem.jpg')
+            } else if (resp > 55){
+                //idoso
+                img.setAttribute('src', 'imagens/idoso-homem.jpg')
+            }
+    } else {
+        genero = 'Mulher'
+        if (resp >= 0 && resp <= 10){
+            //criança
+            img.setAttribute('src', 'imagens/criança-mulher.jpg')
+        } else if (resp < 21){
+            //jovem
+            img.setAttribute('src', 'imagens/jovem-mulher.jpg')
+        } else if (resp < 55){
+            //adulto
+            img.setAttribute('src', 'imagens/jovem-mulher.jpg')
+        } else if (resp > 55){
+            //idoso
+            img.setAttribute('src', 'imagens/idosa-mulher.jpg')
+        }
+    }
+    img.style.width = '250px'
+    img.style.height = '250px'
+    img.style.borderRadius = '50%'
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectei um(a) ${genero } de ${resp} anos.`
+    res.appendChild(img)
 } 
