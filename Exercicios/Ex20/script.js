5ecf58d6413748835dc8aad8ee8216d932067d3b
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2023-05-20 08:10:02 -0300

    Update script.js

--- Exercicios/Ex20/script.js
+++ Exercicios/Ex20/script.js
@@ -1,9 +1,37 @@
 var saida = document.getElementById('res')
 function processar(){
-    var mes = String(document.getElementById('itexto'))
+    var mes = document.getElementById('itexto').value
     switch (mes.toUpperCase()){
         case 'JANEIRO':
-            saida.innerHTML = `ae carai`
-            break
+        case 'FEVEREIRO':
+            saida.innerHTML = `No mês de <mark><strong>${mes}</strong></mark>, estamos na estação do <mark><strong>VERÃO</strong></mark>.`
+            break;
+        case 'MARÇO':
+            saida.innerHTML = `No mês de <mark><strong>${mes}</strong></mark>, estamos na estação do <mark><strong>VERÃO</strong></mark> mas também começa o <mark><strong>OUTONO</strong></mark>.`
+            break;
+        case 'ABRIL':
+        case 'MAIO':
+            saida.innerHTML = `No mês de <mark><strong>${mes}</strong></mark>, estamos na estação do <mark><strong>OUTONO</strong></mark>.`
+            break;
+        case 'JUNHO':
+            saida.innerHTML = `No mês de <mark><strong>${mes}</strong></mark>, estamos na estação do <mark><strong>OUTONO</strong></mark> mas também começa o <mark><strong>INVERNO</strong></mark>.`
+            break;
+        case 'JULHO':
+        case 'AGOSTO':
+            saida.innerHTML = `No mês de <mark><strong>${mes}</strong></mark>, estamos na estação do <mark><strong>INVERNO</strong></mark>.`
+            break;
+        case 'SETEMBRO':
+            saida.innerHTML = `No mês de <mark><strong>${mes}</strong></mark>, estamos na estação do <mark><strong>INVERNO</strong></mark> mas também começa a <mark><strong>PRIMAVERA</strong></mark>.`
+            break;
+        case 'OUTUBRO':
+        case 'NOVEMBRO':
+            `No mês de <mark><strong>${mes}</strong></mark>, estamos na estação da <mark><strong>PRIMAVERA</strong></mark>.`
+            break;
+        case 'DEZEMBRO':
+            saida.innerHTML = `No mês de <mark><strong>${mes}</strong></mark>, estamos na estação da <mark><strong>PRIMAVERA</strong></mark> mas também começa o <mark><strong>VERÃO</strong></mark>.`
+        break;
+        default:
+            saida.innerHTML = `[ERROR] Por favor insira um valor válido.`
+
     }
 }
