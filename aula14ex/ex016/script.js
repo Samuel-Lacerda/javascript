d837ef0a284ae1ccae5924cce70e475ca7915814
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2023-05-22 15:05:23 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -6,14 +6,47 @@ function contar(){
     var passo = Number(document.getElementById('passo').value)
 
     res.innerHTML = ` `
-    while (inicio <= fim){
-        res.innerHTML += `${inicio}`
-        inicio = inicio + passo
-        if (inicio < fim + 1){
-            res.innerHTML += ` &#10140 `
-        } else {
-            res.innerHTML += ` Fim!`
+    if (inicio == 0 && fim == 0 && passo == 0){
+        window.alert('Por favor, preencha todos os campos.')
+        res.innerHTML = `Preparando a contagem...`
+    } else if (fim > 0){
+        while (inicio <= fim){
+            res.innerHTML += `${inicio}`
+            inicio = inicio + passo
+            if (inicio < fim + 1){
+                res.innerHTML += ` &#10140 `
+            } else {
+                res.innerHTML += ` Fim!`
+            }
+        }
+    } else if (fim < 0){
+        while(fim >= inicio){
+            res.innerHTML += `${inicio}`
+            inicio = inicio + passo
+            if (fim < inicio + 1){
+                res.innerHTML += ` &#10140 `
+            } else {
+                res.innerHTML += ` Fim!`
+            }
         }
     }
+    
+    
+
+
+
+  /*else {
+        while (inicio <= fim){
+            res.innerHTML += `${inicio}`
+            inicio = inicio + passo
+            if (inicio < fim + 1){
+                res.innerHTML += ` &#10140 `
+            } else {
+                res.innerHTML += ` Fim!`
+            }
+        }
+    }*/
+        
+    
 
 }
